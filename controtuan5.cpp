2076418d61801d40301c1b89e8a0d4f2afd4f547
20021423 <tuoitretaicao1107@gmail.com>
2021-03-17 19:06:12 +0700

    tuan 5

---
+++ controtuan5.cpp
@@ -0,0 +1,18 @@
+int* getPointerToTen()
+{
+    int* p = new int(10);
+    return p;
+}
+
+int* getPointerToTen()
+{
+    int* p = new int(10);
+    return p;
+}
+
+double* getSquare(double number)
+{
+    double* p = new double(number);
+    *p *= *p;
+    return p;
+}
